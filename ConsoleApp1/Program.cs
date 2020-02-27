300a13f87337647bde305f0fdcb9c600878ba366
= <=>
2020-02-26 23:14:00 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("second line of code");
+            Console.WriteLine("third line of code");
+            Console.WriteLine("fourth line of code");
+        }
+    }
+}
